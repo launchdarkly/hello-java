b01822485fc8cc26de1f15457816c1254cff8a64
John Kodumal <john@launchdarkly.com>
2015-10-06 12:59:24 -0700

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,25 @@
+import com.launchdarkly.client.LDClient;
+import com.launchdarkly.client.LDUser;
+
+import java.util.Arrays;
+
+public class Hello {
+
+ public static void main(String... args) {
+   LDClient client = new LDClient("YOUR_API_KEY");
+
+   LDUser user = new LDUser.Builder("bob@example.com")
+                           .firstName("Bob")
+                           .lastName("Loblaw")
+                           .custom("groups", Arrays.asList("beta_testers"))
+                           .build();
+
+   boolean showFeature = client.toggle("YOUR_FEATURE_KEY", user, false);
+
+   if (showFeature) {
+    System.out.println("Showing your feature");
+   } else {
+    System.out.println("Not showing your feature");
+   }
+ }
+}
