09aee62f162eac54e17d9140260a6585ad060e81
Todd Anderson <tanderson@launchdarkly.com>
2024-03-01 13:57:35 -0600

    feature: application now stays running and prints flag changes in real time

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 import com.launchdarkly.sdk.*;
 import com.launchdarkly.sdk.server.*;
 
@@ -15,37 +17,63 @@ public class Hello {
   }
 
   public static void main(String... args) throws Exception {
-    if (SDK_KEY.equals("")) {
-      showMessage("Please edit Hello.java to set SDK_KEY to your LaunchDarkly SDK key first");
-      System.exit(1);
-    }
-
     LDConfig config = new LDConfig.Builder().build();
 
-    LDClient client = new LDClient(SDK_KEY, config);
-
+    final LDClient client = new LDClient(SDK_KEY, config);
     if (client.isInitialized()) {
       showMessage("SDK successfully initialized!");
     } else {
       showMessage("SDK failed to initialize");
       System.exit(1);
     }
-    
-    // Set up the evaluation context. This context should appear on your LaunchDarkly contexts
-    // dashboard soon after you run the demo.
-    LDContext context = LDContext.builder("example-user-key")
-                            .name("Sandy")
-                            .build();
 
+    // Set up the evaluation context. This context should appear on your
+    // LaunchDarkly contexts dashboard soon after you run the demo.
+    final LDContext context = LDContext.builder("example-user-key")
+        .name("Sandy")
+        .build();
+
+    // Evaluate the feature flag for this context.
     boolean flagValue = client.boolVariation(FEATURE_FLAG_KEY, context, false);
-
     showMessage("Feature flag '" + FEATURE_FLAG_KEY + "' is " + flagValue + " for this context");
 
-    // Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
-    // events to LaunchDarkly before the program exits. If analytics events are not delivered,
-    // the context attributes and flag usage statistics will not appear on your dashboard. In
-    // a normal long-running application, the SDK would continue running and events would be
-    // delivered automatically in the background.
-    client.close();
+    // Here we request that the SDK flush pending analytic events so that you see
+    // data for the above evaluation on the dashboard immediatelynow rather than
+    // at the next automatic flush interval. You don't need to do this under normal
+    // circumstances in your own application.
+    client.flush();
+
+    // We set up a flag change listener so you can see flag changes as you change
+    // the flag rules.
+    client.getFlagTracker().addFlagChangeListener(event -> {
+      showMessage("Feature flag '" + event.getKey() + "' has changed.");
+      if (event.getKey().equals(FEATURE_FLAG_KEY)) {
+        boolean value = client.boolVariation(FEATURE_FLAG_KEY, context, false);
+        showMessage("Feature flag '" + FEATURE_FLAG_KEY + "' is " + value + " for this context");
+      }
+    });
+    showMessage("Listening for feature flag changes.  Use Ctrl+C to terminate.");
+
+    // Here we ensure that when the application terminates, the SDK shuts down
+    // cleanly and has a chance to deliver analytics events to LaunchDarkly.
+    // If analytics events are not delivered, the context attributes and flag usage
+    // statistics may not appear on your dashboard. In a normal long-running
+    // application, the SDK would continue running and events would be delivered
+    // automatically in the background.
+    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+      public void run() {
+        try {
+          client.close();
+        } catch (IOException e) {
+          // ignore
+        }
+      }
+    }, "ldclient-cleanup-thread"));
+
+    // Keeps example application alive.
+    Object mon = new Object();
+    synchronized (mon) {
+      mon.wait();
+    }
   }
 }
