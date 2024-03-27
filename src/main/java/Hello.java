a8f274bbc78df99d6375dcba2daa7263d2027b54
Louis Chan <lchan@launchdarkly.com>
2024-03-26 17:40:39 -0700

    feat: hello app proposal

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,11 +9,20 @@ public class Hello {
   static final String SDK_KEY = "";
 
   // Set FEATURE_FLAG_KEY to the feature flag key you want to evaluate.
-  static final String FEATURE_FLAG_KEY = "my-boolean-flag";
+  static final String FEATURE_FLAG_KEY = "sample-feature";
   
   private static void showMessage(String s) {
     System.out.println("*** " + s);
     System.out.println();
+  }
+
+  private static void printLogo() {
+    showMessage("\n  _                           _     ____             _    _       \n" + //
+                " | |    __ _ _   _ _ __   ___| |__ |  _ \\  __ _ _ __| | _| |_   _ \n" + //
+                " | |   / _` | | | | '_ \\ / __| '_ \\| | | |/ _` | '__| |/ / | | | |\n" + //
+                " | |__| (_| | |_| | | | | (__| | | | |_| | (_| | |  |   <| | |_| |\n" + //
+                " |_____\\__,_|\\__,_|_| |_|\\___|_| |_|____/ \\__,_|_|  |_|\\_\\_|\\__, |\n" + //
+                "                                                            |___/ ");
   }
 
   public static void main(String... args) throws Exception {
@@ -37,11 +46,9 @@ public class Hello {
     boolean flagValue = client.boolVariation(FEATURE_FLAG_KEY, context, false);
     showMessage("Feature flag '" + FEATURE_FLAG_KEY + "' is " + flagValue + " for this context");
 
-    // Here we request that the SDK flush pending analytic events so that you see
-    // data for the above evaluation on the dashboard immediatelynow rather than
-    // at the next automatic flush interval. You don't need to do this under normal
-    // circumstances in your own application.
-    client.flush();
+    if (flagValue) {
+      printLogo();
+    }
 
     // We set up a flag change listener so you can see flag changes as you change
     // the flag rules.
@@ -50,6 +57,10 @@ public class Hello {
       if (event.getKey().equals(FEATURE_FLAG_KEY)) {
         boolean value = client.boolVariation(FEATURE_FLAG_KEY, context, false);
         showMessage("Feature flag '" + FEATURE_FLAG_KEY + "' is " + value + " for this context");
+
+        if (value) {
+          printLogo();
+        }
       }
     });
     showMessage("Listening for feature flag changes.  Use Ctrl+C to terminate.");
